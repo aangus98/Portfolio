7aea7f5a4a6adece1af713c783c279a131b839fe
Andrew <mrangus298@gmail.com>
2025-02-06 10:39:50 -0600

    Added icons for the github, linkedin, and goodreads links

--- vite-project/src/components/Footer.tsx
+++ vite-project/src/components/Footer.tsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { TbBrandLinkedin } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa";
+import { FaGoodreads } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} Andrew Angus Portfolio</p>
       <ul>
-        <a href="https://github.com/aangus98">Github</a>
+        <a href="https://github.com/aangus98"><FaGithub /></a>
       </ul>
       <ul>  
-        <a href="https://www.linkedin.com/in/andrew-angus-3404551a5/">Linkedin</a>
+        <a href="https://www.linkedin.com/in/andrew-angus-3404551a5/"><TbBrandLinkedin /></a>
       </ul>
       <ul>
-        <a href="https://www.goodreads.com/user/show/174972405-andrew-angus">Goodreads</a>
+        <a href="https://www.goodreads.com/user/show/174972405-andrew-angus"><FaGoodreads /></a>
       </ul>
     </footer>
   );
